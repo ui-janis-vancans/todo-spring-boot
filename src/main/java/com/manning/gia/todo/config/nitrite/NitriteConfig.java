1e65cc5a1251957ff11b2adc30b0dbc74a8450ed
Kamil Kos <kamil.kos@ui.com>
2020-09-24 13:45:17 +0200

    feat: nitrite (#4)
    * feat: add support for nitrite
    
    * feat: fix saving and getting objects for nitrite

---
+++ src/main/java/com/manning/gia/todo/config/nitrite/NitriteConfig.java
@@ -0,0 +1,21 @@
+package com.manning.gia.todo.config.nitrite;
+
+import com.manning.gia.todo.DatabaseProfiles;
+import org.dizitart.no2.Nitrite;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile(DatabaseProfiles.NITRITE)
+public class NitriteConfig {
+
+    @Bean
+    Nitrite nitriteDatabase() {
+        Nitrite db = Nitrite.builder()
+                .filePath("/tmp/test.db")
+                .openOrCreate();
+        Runtime.getRuntime().addShutdownHook(new Thread(db::close));
+        return db;
+    }
+}
