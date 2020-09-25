0552f0e3eeb6bfed2264293716dec58890f3e879
Janis Vancans <janis.vancans@ui.com>
2020-09-25 09:51:21 +0300

    add grafana

---
+++ src/main/java/com/manning/gia/todo/config/AppMetricsConfig.java
@@ -0,0 +1,21 @@
+package com.manning.gia.todo.config;
+
+import io.github.mweirauch.micrometer.jvm.extras.ProcessMemoryMetrics;
+import io.github.mweirauch.micrometer.jvm.extras.ProcessThreadMetrics;
+import io.micrometer.core.instrument.binder.MeterBinder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppMetricsConfig {
+
+    @Bean
+    public MeterBinder processMemoryMetrics() {
+        return new ProcessMemoryMetrics();
+    }
+
+    @Bean
+    public MeterBinder processThreadMetrics() {
+        return new ProcessThreadMetrics();
+    }
+}
